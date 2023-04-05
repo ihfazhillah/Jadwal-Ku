da74032659571d2ebdef13ce251e68379a4f9b26
ihfazhillah <mihfazhillah@gmail.com>
2023-04-06 00:41:00 +0700

    Initiate login view

---
+++ app/src/main/java/com/ihfazh/jadwal_ku/screens/login/LoginViewMvc.kt
@@ -0,0 +1,73 @@
+package com.ihfazh.jadwal_ku.screens.login
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.ihfazh.jadwal_ku.R
+import com.ihfazh.jadwal_ku.screens.common.views.BaseMvcView
+
+
+data class LoginData(
+    val username: String,
+    val password: String
+)
+
+
+
+class LoginViewMvc(
+    inflater: LayoutInflater,
+    parent: ViewGroup?
+
+) : BaseMvcView<LoginViewMvc.Listener>(inflater, parent, R.layout.layout_login) {
+
+    interface Listener {
+        fun onLoginClick()
+    }
+
+    private val edtUsernameLayout: TextInputLayout = findViewById(R.id.edtUsernameLayout)
+    private val edtPasswordLayout: TextInputLayout = findViewById(R.id.edtPasswordLayout)
+
+    private val edtUsername: TextInputEditText = findViewById(R.id.edtUsername)
+    private val edtPassword: TextInputEditText = findViewById(R.id.edtPassword)
+    private val btnLogin: MaterialButton = findViewById(R.id.btnLogin)
+
+    private val txtError: TextView = findViewById(R.id.txtError)
+
+    init {
+        btnLogin.setOnClickListener {
+            listeners.forEach { listener -> listener.onLoginClick() }
+        }
+    }
+
+    fun getLoginData(): LoginData {
+        val username = edtUsername.text.toString()
+        val password = edtPassword.text.toString()
+        return LoginData(username, password)
+    }
+
+    fun setUsernameError(error: String){
+        edtUsernameLayout.error = error
+    }
+
+    fun setPasswordError(error: String){
+        edtPasswordLayout.error = error
+    }
+
+    fun setLoggingIn(flag: Boolean){
+        btnLogin.isEnabled = flag
+    }
+
+    fun setAndDisplayGlobalError(error: String){
+        txtError.text = error
+        txtError.visibility = View.VISIBLE
+    }
+
+    fun hideGlobalError(){
+        txtError.visibility = View.GONE
+    }
+
+}
