1e5e18e2c081d1e9c65f17dcf495daa662577756
ihfazhillah <mihfazhillah@gmail.com>
2023-04-09 12:17:51 +0700

    fix listener for fragment change

--- app/src/main/java/com/ihfazh/jadwal_ku/screens/common/fragmentframehelper/FragmentChangeHelper.kt
+++ app/src/main/java/com/ihfazh/jadwal_ku/screens/common/fragmentframehelper/FragmentChangeHelper.kt
@@ -2,30 +2,28 @@ package com.ihfazh.jadwal_ku.screens.common.fragmentframehelper
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentManager.FragmentLifecycleCallbacks
 import com.ihfazh.jadwal_ku.common.BaseObservable
 import com.ihfazh.jadwal_ku.dependencyinjection.activity.ActivityScope
 import javax.inject.Inject
 
 @ActivityScope
 class FragmentChangeHelper @Inject constructor(
-    private val fragmentManager: FragmentManager,
-    private val fragmentFrameWrapper: FragmentFrameWrapper
+    fragmentManager: FragmentManager,
 ): BaseObservable<FragmentChangeHelper.Listener>() {
     interface Listener {
        fun onFragmentChanged(fragment: Fragment)
     }
 
     init {
-        fragmentManager.addOnBackStackChangedListener {
-            val currentFragment = getCurrentFragment()
-            if (currentFragment != null){
-                listeners.forEach {  listener -> listener.onFragmentChanged(currentFragment) }
+
+        fragmentManager.registerFragmentLifecycleCallbacks(object : FragmentLifecycleCallbacks() {
+            override fun onFragmentStarted(fm: FragmentManager, f: Fragment) {
+                listeners.forEach {  listener -> listener.onFragmentChanged(f) }
             }
 
-        }
+        }, false)
+
     }
 
-    private fun getCurrentFragment() : Fragment? = fragmentManager.findFragmentById(fragmentFrameId)
-
-    private val fragmentFrameId get() = fragmentFrameWrapper.fragmentFrame.id
 }
